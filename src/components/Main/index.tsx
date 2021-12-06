85596dd0d772f7332713d4e3f9ac89db807d2ea1
Yuri Burk <yuriburk@gmail.com>
2021-12-06 20:35:15 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,9 @@
+import * as SC from './styles'
+
+const Main = () => (
+  <SC.Container>
+    <SC.Title>Burk.dev</SC.Title>
+  </SC.Container>
+)
+
+export default Main
---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyle from 'styles/global'
+import { theme } from 'styles/theme'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Burk.dev Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#05092B" />
+        <meta name="description" content="A boilerplate with Next JS" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
